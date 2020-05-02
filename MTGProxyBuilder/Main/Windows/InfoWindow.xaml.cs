9ecf6c68f3057be358a4c26665f29b981d4cd267
Lukas Kern <kern775@gmail.com>
2020-05-02 04:13:26 +0200

    Reworked ParseDecklist method Added multithreading Updated InfoWindow Other minor changes

--- MTGProxyBuilder/Main/Windows/InfoWindow.xaml.cs
+++ MTGProxyBuilder/Main/Windows/InfoWindow.xaml.cs
@@ -1,12 +1,36 @@
-﻿using System.Windows;
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace MTGProxyBuilder
 {
 	public partial class InfoWindow : Window
 	{
-        public InfoWindow()
+		[DllImport("user32.dll")]
+		static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+		[DllImport("user32.dll")]
+		static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+
+		public InfoWindow()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnSourceInitialized(EventArgs e)
+		{
+			base.OnSourceInitialized(e);
+			IntPtr hwnd = new WindowInteropHelper(this).Handle;
+			IntPtr hMenu = GetSystemMenu(hwnd, false);
+			if (hMenu != IntPtr.Zero)
+				EnableMenuItem(hMenu, 0xF060, 0x00000000 | 0x00000001);
+		}
+
+		private void InfoWindowClosing(object sender, CancelEventArgs e)
         {
-            InitializeComponent();
+	        e.Cancel = true;
         }
 	}
 }
--- MTGProxyBuilder/Main/Windows/MainWindow.xaml.cs
+++ MTGProxyBuilder/Main/Windows/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +25,7 @@ namespace MTGProxyBuilder
 		private List<CardAmount> CardsWithAmounts;
 		private Settings UserSettings = Properties.Settings.Default;
 		private InfoWindow Info = new InfoWindow();
+		private string PDFOutputPath;
 
 		public MainWindow()
 		{
@@ -34,7 +34,8 @@ namespace MTGProxyBuilder
 
 		public List<CardAmount> InterpretCardlist()
 		{
-			string[] cards = Decklist.Text.Split(new []{ NewLine }, StringSplitOptions.RemoveEmptyEntries);
+			string[] cards = null;
+			Dispatcher.Invoke(() => { cards = Decklist.Text.Split(new[] { NewLine }, StringSplitOptions.RemoveEmptyEntries); });
 			cards.ToList().ForEach(e => e.Trim());
 
 			List<CardAmount> cardAmounts = new List<CardAmount>();
@@ -55,91 +56,11 @@ namespace MTGProxyBuilder
 			return cardAmounts;
 		}
 
-		private void CreatePDFButtonClicked(object sender, RoutedEventArgs eventArgs)
+		private async void CreatePDFButtonClicked(object sender, RoutedEventArgs eventArgs)
 		{
-			IsEnabled = false;
-			bool defaultDirectory = string.IsNullOrEmpty(UserSettings.DefaultOutputDirectory);
 			string fileName = UserSettings.DefaultFilename.EndsWith(".pdf") ? UserSettings.DefaultFilename : UserSettings.DefaultFilename + ".pdf";
 			CommonOpenFileDialog dialog = null;
-
-			Task backgroundWorker = new Task(() =>
-			{
-				List<byte[]> images = new List<byte[]>();
-				WebClient webClient = new WebClient();
-
-				Info.Show();
-				Info.Owner = this;
-				IsEnabled = false;
-
-				foreach (CardAmount card in CardsWithAmounts)
-				{
-					Info.TextBlock.Text = "Fetching Images, Progress: " + images.Count + "/" + CardsWithAmounts.Count;
-
-					string selEdition = card.SelectedEdition.TrimStart('[').Split(',')[0];
-					byte[] img = webClient.DownloadData(card.EditionNamesArtworkURLs.Find(e => e.Key == selEdition).Value);
-					byte[] backFace = !string.IsNullOrEmpty(card.BackFaceURL) ? webClient.DownloadData(card.BackFaceURL) : null;
-
-					for (int j = 0; j < card.Amount; j++)
-					{
-						images.Add(img);
-						if (backFace != null)
-							images.Add(backFace);
-					}
-
-					Thread.Sleep(100);
-				}
-
-				PdfDocument pdf = new PdfDocument();
-				PdfPage page = new PdfPage(pdf);
-				XGraphics draw = XGraphics.FromPdfPage(page);
-
-				float cardWidth = 178 * (UserSettings.ProxySizePercentage / 100f);
-				float cardHeight = 249 * (UserSettings.ProxySizePercentage / 100f);
-				int widthGap = UserSettings.GapX;
-				int heightGap = UserSettings.GapY;
-
-				for (int i = 0; i < Math.Ceiling(images.Count / 9f); i++)
-				{
-					float x = 0;
-					float y = 0;
-
-					int remainingImages = images.Count - i * 9;
-					if (remainingImages > 9)
-						remainingImages = 9;
-
-					for (int j = 1; j < remainingImages + 1; j++)
-					{
-						MemoryStream mem = new MemoryStream(images.Skip(i * 9).ToList()[j - 1]);
-						draw.DrawImage(XImage.FromStream(mem), x, y, cardWidth, cardHeight);
-						draw.Save();
-						if (j % 3 != 0)
-							x += cardWidth + widthGap;
-						else
-						{
-							x = 0;
-							y += cardHeight + heightGap;
-						}
-					}
-
-					x = 0;
-					y = 0;
-
-					pdf.AddPage(page);
-					page = new PdfPage(pdf);
-					draw = XGraphics.FromPdfPage(page);
-				}
-
-				if(defaultDirectory)
-					pdf.Save(dialog.FileName + Path.DirectorySeparatorChar + fileName);
-				else
-					pdf.Save(UserSettings.DefaultOutputDirectory + Path.DirectorySeparatorChar + fileName);
-				
-				pdf.Close();
-				Info.Close();
-				IsEnabled = true;
-			});
-
-			if (defaultDirectory)
+			if (string.IsNullOrEmpty(UserSettings.DefaultOutputDirectory))
 			{
 				dialog = new CommonOpenFileDialog
 				{
@@ -147,14 +68,99 @@ namespace MTGProxyBuilder
 					IsFolderPicker = true
 				};
 				if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-					backgroundWorker.RunSynchronously();
-				else
-					Focus();
+				{
+					PDFOutputPath = dialog.FileName + Path.DirectorySeparatorChar + fileName;
+					await Task.Run(() => PrintPDF());
+				}
+				else Focus();
 			}
 			else
-				backgroundWorker.RunSynchronously();
+			{
+				PDFOutputPath = UserSettings.DefaultOutputDirectory + Path.DirectorySeparatorChar + fileName;
+				await Task.Run(() => PrintPDF());
+			}
+		}
 
-			IsEnabled = true;
+		private void PrintPDF()
+		{
+			List<byte[]> images = new List<byte[]>();
+			WebClient webClient = new WebClient();
+
+			Dispatcher.Invoke(() => 
+			{
+				Info.Show();
+				Info.Owner = this;
+				IsEnabled = false;
+			});
+
+			foreach (CardAmount card in CardsWithAmounts)
+			{
+				Dispatcher.Invoke(() => { Info.TextBlock.Text = "Fetching Images, Progress: " + images.Count + "/" + CardsWithAmounts.Count; });
+
+				string selEdition = card.SelectedEdition.TrimStart('[').Split(',')[0];
+				byte[] img = webClient.DownloadData(card.EditionNamesArtworkURLs.Find(e => e.Key == selEdition).Value);
+				byte[] backFace = !string.IsNullOrEmpty(card.BackFaceURL) ? webClient.DownloadData(card.BackFaceURL) : null;
+
+				for (int j = 0; j < card.Amount; j++)
+				{
+					images.Add(img);
+					if (backFace != null)
+						images.Add(backFace);
+				}
+
+				Thread.Sleep(100);
+			}
+
+			Dispatcher.Invoke(() => { Info.TextBlock.Text = "Building PDF..."; });
+
+			PdfDocument pdf = new PdfDocument();
+			PdfPage page = new PdfPage(pdf);
+			XGraphics draw = XGraphics.FromPdfPage(page);
+
+			float cardWidth = 178 * (UserSettings.ProxySizePercentage / 100f);
+			float cardHeight = 249 * (UserSettings.ProxySizePercentage / 100f);
+			int widthGap = UserSettings.GapX;
+			int heightGap = UserSettings.GapY;
+
+			for (int i = 0; i < Math.Ceiling(images.Count / 9f); i++)
+			{
+				float x = 0;
+				float y = 0;
+
+				int remainingImages = images.Count - i * 9;
+				if (remainingImages > 9)
+					remainingImages = 9;
+
+				for (int j = 1; j < remainingImages + 1; j++)
+				{
+					MemoryStream mem = new MemoryStream(images.Skip(i * 9).ToList()[j - 1]);
+					draw.DrawImage(XImage.FromStream(mem), x, y, cardWidth, cardHeight);
+					draw.Save();
+					if (j % 3 != 0)
+						x += cardWidth + widthGap;
+					else
+					{
+						x = 0;
+						y += cardHeight + heightGap;
+					}
+				}
+
+				x = 0;
+				y = 0;
+
+				pdf.AddPage(page);
+				page = new PdfPage(pdf);
+				draw = XGraphics.FromPdfPage(page);
+			}
+
+			pdf.Save(PDFOutputPath);
+			pdf.Close();
+
+			Dispatcher.Invoke(() =>
+			{
+				Info.Hide();
+				IsEnabled = true;
+			});
 		}
 
 		private void OpenSettingsWindow(object sender, RoutedEventArgs e)
@@ -170,72 +176,83 @@ namespace MTGProxyBuilder
 			MessageBox.Show("This function is not implemented.", "Not implemented");
 		}
 
-		private void CustomizeCardsClicked(object sender, RoutedEventArgs eventArgs)
+		private async void CustomizeCardsClicked(object sender, RoutedEventArgs eventArgs)
 		{
-			Task t = new Task(async () =>
+			if (CardGrid.Items.Count != 0)
 			{
-				Info.Show();
+				if (MessageBox.Show("There seems to be data in the grid. Do you want to override it?",
+				                    "Override data?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+					await Task.Run(() => ParseDecklist());
+			}
+			else await Task.Run(() => ParseDecklist());
+		}
+
+		private async void ParseDecklist()
+		{
+			Dispatcher.Invoke(() => 
+			{ 
 				Info.Owner = this;
 				Info.TextBlock.Text = "Parsing decklist...";
 				IsEnabled = false;
+				Info.Show();
+			});
 
-				HttpResponseMessage resp = await PullAllDecklistData();
-				JObject jo = JObject.Parse(resp.Content.ReadAsStringAsync().Result);
-				JToken data = jo["data"];
-				List<CardAmount> allCards = InterpretCardlist();
+			HttpResponseMessage resp = await PullAllDecklistData();
+			JObject jo = JObject.Parse(resp.Content.ReadAsStringAsync().Result);
+			JToken data = jo["data"];
+			List<CardAmount> allCards = InterpretCardlist();
 
-				//foreach (CardAmount currentCard in allCards)
-				//{
-
-				//}
-
-				foreach (JToken jt in data.Children()) //TODO Rewrite Loop
+			//["promo_types"] prerelease promostamped showcase nyxtouched judgegift ["full_art"] ["textless"]
+			//https://scryfall.com/docs/api/layouts - Frame Effects
+			//https://scryfall.com/docs/api/cards
+			foreach (CardAmount currentCard in allCards)
+			{
+				foreach (JToken jt in data.Children())
 				{
-					HttpResponseMessage prints_search_uri_resp = await APIInterface.Get("/cards/search",
-						jt["prints_search_uri"].Value<string>().Split('?')[1]);
-					JToken prints_search_uri = JToken.Parse(prints_search_uri_resp.Content.ReadAsStringAsync().Result);
-					List<KeyValuePair<string, string>> editionNames = new List<KeyValuePair<string, string>>();
-					List<string> cardNames = jt["name"].Value<string>().Split(new []{" // "}, StringSplitOptions.RemoveEmptyEntries).ToList();
-					CardAmount currentCard = allCards.Find(card => cardNames.Contains(card.CardName, StringComparer.OrdinalIgnoreCase));
-					foreach (JToken psu in prints_search_uri["data"])
+					List<string> cardNames = jt["name"].Value<string>().Split(new[] { " // " }, StringSplitOptions.RemoveEmptyEntries).ToList();
+					if (cardNames.Contains(currentCard.CardName, StringComparer.OrdinalIgnoreCase))
 					{
-						string image_uri = psu["image_uris"] != null ? psu["image_uris"]["png"].Value<string>() :
-								           psu["card_faces"][0]["image_uris"]["png"].Value<string>();
-						editionNames.Add(new KeyValuePair<string, string>(psu["set_name"].Value<string>() + 
-										" (" + psu["set"].Value<string>().ToUpper() + ")", image_uri));
-						if (psu["card_faces"] != null && psu["layout"].Value<string>() == "transform")
-							currentCard.BackFaceURL = psu["card_faces"][1]["image_uris"]["png"].Value<string>();
+						HttpResponseMessage allPrintsResp = await APIInterface.Get("/cards/search",
+							jt["prints_search_uri"].Value<string>().Split('?')[1]);
+						JToken allPrints = JToken.Parse(allPrintsResp.Content.ReadAsStringAsync().Result);
+						List<KeyValuePair<string, string>> editionNames = new List<KeyValuePair<string, string>>();
+						foreach (JToken singlePrint in allPrints["data"])
+						{
+							string image_uri = singlePrint["image_uris"] != null ? singlePrint["image_uris"]["png"].Value<string>() :
+													   singlePrint["card_faces"][0]["image_uris"]["png"].Value<string>();
+							editionNames.Add(new KeyValuePair<string, string>(singlePrint["set_name"].Value<string>() +
+																			  " (" + singlePrint["set"].Value<string>().ToUpper() + ")", image_uri));
+							if (singlePrint["layout"].Value<string>() == "transform") //impl meld layout
+								currentCard.BackFaceURL = singlePrint["card_faces"][1]["image_uris"]["png"].Value<string>();
+						}
+						currentCard.SelectedEdition = editionNames[0].Key;
+						currentCard.EditionNamesArtworkURLs = editionNames;
+						currentCard.DisplayName = cardNames.Count > 1 ? cardNames[0] + " // " + cardNames[1] : cardNames[0];
+						break;
 					}
-					currentCard.SelectedEdition = editionNames[0].Key;
-					currentCard.EditionNamesArtworkURLs = editionNames;
-					currentCard.DisplayName = cardNames.Count > 1 ? cardNames[0] + " // " + cardNames[1] : cardNames[0];
 				}
-				allCards.RemoveAll(e => e.EditionNamesArtworkURLs == null);
-				CardsWithAmounts = allCards;
+			}
+
+			allCards.RemoveAll(e => e.EditionNamesArtworkURLs == null);
+			CardsWithAmounts = allCards;
+
+			Dispatcher.Invoke(() =>
+			{
 				CardGrid.ItemsSource = CardsWithAmounts;
 				CardGrid.Items.Refresh();
 				CreatePDFButton.IsEnabled = CardGrid.Items.Count != 0;
 				Info.Hide();
 				IsEnabled = true;
-
-				string notFoundList = jo["not_found"].Children().Aggregate("", (s, token) => s + "\"" + token["name"] + "\", ").TrimEnd(',', ' ');
-				if (!string.IsNullOrEmpty(notFoundList))
-					MessageBox.Show("Cards not found: " + notFoundList, "Cards not found");
 			});
 
-			if (CardGrid.Items.Count != 0)
-			{
-				if (MessageBox.Show("There seems to be data in the grid. Do you want to override it?",
-				                    "Override data?", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-					t.RunSynchronously();
-			}
-			else
-				t.RunSynchronously();
+			string notFoundList = jo["not_found"].Children().Aggregate("", (s, token) => s + "\"" + token["name"] + "\", ").TrimEnd(',', ' ');
+			if (!string.IsNullOrEmpty(notFoundList))
+				MessageBox.Show("Cards not found: " + notFoundList, "Cards not found");
 		}
 
 		private async Task<HttpResponseMessage> PullAllDecklistData()
 		{
-			List<CardAmount> cards = InterpretCardlist();
+			List<CardAmount> cards = InterpretCardlist().GroupBy(e => e.CardName).Select(e => e.First()).ToList();
 			JArray collection = new JArray();
 			foreach (CardAmount ca in cards)
 				collection.Add(JToken.Parse("{\"name\":\"" + ca.CardName + "\"}"));
